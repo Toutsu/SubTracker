bfc7ef616af067d533ac09ccc481419200a14484
Салопаев Алексей Вячеславович (4104369) - ООО ГК <salopaev@dev.vtb.ru>
2025-09-01 14:04:58 +0300

    Update Docker configurations for backend and frontend services. Add Dockerfile for telegram-bot. Update docker-compose and next.config.js

--- web-frontend/next.config.js
+++ web-frontend/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   // Убираем swcMinify, так как это вызывает предупреждение
   experimental: {
     reactCompiler: true,
